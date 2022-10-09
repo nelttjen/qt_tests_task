a0f863a20332941207a55e527a52277ee1dbca9f
NelttjeN <tatyapa0880@yandex.ru>
2022-10-09 21:25:17 +0500

    admin window question import

--- widgets/AdminWindow.py
+++ widgets/AdminWindow.py
@@ -1,10 +1,12 @@
+import json
 import logging
+from pathlib import Path
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QFileDialog
 
 from utils import Strings
-from .util import window_centralizate
+from .util import window_centralizate, show_info, show_warn, show_error
 from .UI import AdminWindowUi
 
 
@@ -12,7 +14,13 @@ class AdminWindow(QDialog, AdminWindowUi):
     def __init__(self, parent):
         super().__init__(parent, Qt.WindowCloseButtonHint)
 
-        self.answers = None
+        self.questions = []
+        self.options = {
+            'password': '',
+            'use_password': False,
+            'exel_export': False,
+            'show_users': False
+        }
         self.initUi()
 
     def initUi(self):
@@ -21,8 +29,61 @@ class AdminWindow(QDialog, AdminWindowUi):
         window_centralizate(self)
         self.setWindowTitle(Strings.admin_window_title)
 
-    def exec_(self) -> list:
+        self.connect_actions()
+
+    def connect_actions(self):
+        self.test_file_choose_btn.clicked.connect(self.choose_file_btn)
+        self.test_file_confirm_btn.clicked.connect(self.confirm_file_btn)
+
+    def choose_file_btn(self):
+        file = QFileDialog.getOpenFileName(parent=self, caption=Strings.AdminUi.file_choose_dialog_caption,
+                                           directory='./', filter='JSON (*.json);;All files (*.*)')[0]
+        f_path = Path(file)
+        if not f_path.is_file():
+            return
+        try:
+            with open(file, 'r', encoding='utf-8') as f_input:
+                data = json.load(f_input)
+        except PermissionError:
+            show_error(self, text=Strings.AdminUi.file_permission_error)
+            return
+        except (json.JSONDecodeError, UnicodeDecodeError):
+            show_error(self, text=Strings.AdminUi.file_read_error)
+            return
+        if not isinstance(data, list) or len(data) == 0:
+            show_warn(self, text=Strings.AdminUi.file_empty_warn)
+            self.set_question_count(0)
+            return
+        checked_questions = []
+        for item in data:
+            # Проверка что есть все ключи и эти ключи правильных типов данных
+            if all([i in item.keys() for i in ['id', 'text', 'answers', 'is_multiple']]) and \
+                all([isinstance(item['id'], int), isinstance(item['text'], str),
+                     isinstance(item['answers'], list), isinstance(item['is_multiple'], bool)]):
+
+                checked_questions.append(item)
+        if len(checked_questions) == 0:
+            show_warn(self, text=Strings.AdminUi.file_empty_warn)
+        self.set_question_count(len(checked_questions))
+        self.questions = checked_questions
+        logging.info(f'File selected, found questions: {len(self.questions)}')
+
+    def confirm_file_btn(self):
+        if self.password_input_checkbox.isChecked() and len(self.password_input.text()) < 3:
+            show_error(self, text=Strings.AdminUi.no_password_error)
+            return
+        self.options['password'] = self.password_input.text()
+        self.options['use_password'] = self.password_input_checkbox.isChecked()
+        self.options['exel_export'] = self.exel_export_checkbox.isChecked()
+        self.options['show_users'] = self.show_complete_info_checkbox.isChecked()
+        self.accept()
+
+    def set_question_count(self, number: int):
+        self.test_file_info_label.setText(f'{Strings.AdminUi.test_file_info_label} {number}')
+        self.test_file_info_label.setAlignment(Qt.AlignCenter)
+
+    def exec_(self) -> tuple:
         super(AdminWindow, self).exec_()
         logging.info(f'AdminWindow accepted, returning '
-                     f'{len(self.answers) if isinstance(self.answers, list) else 0} questions')
-        return self.answers or []
+                     f'{len(self.questions)} questions')
+        return self.questions, self.options
--- widgets/util.py
+++ widgets/util.py
@@ -1,10 +1,15 @@
 import screeninfo
 
 from PyQt5.QtGui import QFont
-
+from PyQt5.QtWidgets import QMessageBox
 
 monitor = screeninfo.get_monitors()[0]
 user_res_w, user_res_h = monitor.width, monitor.height
+
+centralizate = lambda win_w, obj_w: int(win_w // 2 - obj_w // 2)
+window_centralizate = lambda win: win.move(centralizate(user_res_w, win.width()),
+                                           centralizate(user_res_h, win.height()))
+
 
 def get_font(size: int = 8):
     font = QFont()
@@ -12,6 +17,13 @@ def get_font(size: int = 8):
     return font
 
 
-centralizate = lambda win_w, obj_w: int(win_w // 2 - obj_w // 2)
-window_centralizate = lambda win: win.move(centralizate(user_res_w, win.width()),
-                                           centralizate(user_res_h, win.height()))
+def show_info(parrent, caption='Info', text=''):
+    QMessageBox.information(parrent, caption, text, QMessageBox.Ok)
+
+
+def show_warn(parrent, caption='Warning', text=''):
+    QMessageBox.warning(parrent, caption, text, QMessageBox.Ok)
+
+
+def show_error(parrent, caption='Error', text=''):
+    QMessageBox.critical(parrent, caption, text, QMessageBox.Ok)
